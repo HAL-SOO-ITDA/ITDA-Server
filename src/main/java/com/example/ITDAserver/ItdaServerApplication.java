cfcbcb69b7812248ccad8e46188c958a4c55c6d1
iamsoojung <crystar2402@naver.com>
2023-10-10 22:35:35 +0900

    Initial commit

---
+++ src/main/java/com/example/ITDAserver/ItdaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ITDAserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItdaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItdaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ITDAserver/ItdaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ITDAserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItdaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
